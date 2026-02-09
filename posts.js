bc20d8201374b401391463504599ce2ea993cbfc
mahamudul hasan seber <sebermh@gmail.com>
2026-02-09 14:40:09 +0600

    Display Post Card & Style CSS

--- posts.js
+++ posts.js
@@ -8,35 +8,100 @@ const loadPost = ()=>{
     });
 };
 
+
+// {
+//     "userId": 10,
+//     "id": 96,
+//     "title": "quaerat velit veniam amet cupiditate aut numquam ut sequi",
+//     "body": "in non odio excepturi sint eum\nlabore voluptates vitae quia qui et\ninventore itaque rerum\nveniam non exercitationem delectus aut"
+// }
+
+
 const displayPosts = (posts)=>{
-
-    // 1. get the container
     const postContainer = document.getElementById('post-container');
-    console.log(postContainer);
-
-    // for loop
-
-    // for (let i = 0; i < posts.length; i++){
-    //     console.log(posts[i]);
-    // }
-
-    // for of loop
-
-    // for (const post of posts){
-    //     console.log(post);
-    // }
-
-    // forEach loop
-
+    postContainer.innerHTML = '';
     posts.forEach((post)=>{
-        console.log(post.title);
-        //2. create HTML element
-        const li = document.createElement('li');
-        li.innerText = post.title;
-        console.log(li);
+        const postCard = document.createElement('div');
+        postCard.innerHTML =`
+          <div class="post-card">
+            <h2>${post.title}</h2>
+            <p>${post.body}</p>
+        </div>
+        `
+        postContainer.append(postCard);
+    });
+};
+loadPost();
 
-        //3. add li into container
-        postContainer.appendChild(li);
-    })
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const displayPosts = (posts)=>{
+
+//     // 1. get the container
+//     const postContainer = document.getElementById('post-container');
+//     postContainer.innerHTML = '';
+//     // console.log(postContainer);
+
+//     // for loop
+
+//     for (let i = 0; i < posts.length; i++){
+//         console.log(posts[i]);
+//     }
+
+//     // for of loop
+
+//     for (const post of posts){
+//         console.log(post);
+//     }
+
+//     // forEach loop
+
+//     posts.forEach((post)=>{
+//         console.log(post.title);
+//         //2. create HTML element
+//         const li = document.createElement('li');
+//         li.innerText = post.title;
+//         console.log(li);
+
+//         //3. add li into container
+//         postContainer.appendChild(li);
+//     })
+
+// }
